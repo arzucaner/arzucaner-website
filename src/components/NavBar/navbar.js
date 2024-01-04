d98196f78d9beed49736d05d29bf8f11433ed421
Arzu Caner <arzuguneycaner@gmail.com>
2024-01-04 19:55:10 +0000

    Add intro.js and intro.css

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './navbar.css';
 import logo from '../../assets/logo.png';
-import contactImg from '../../assets/contactme.png';
+import contactImg from '../../assets/contact.png';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
