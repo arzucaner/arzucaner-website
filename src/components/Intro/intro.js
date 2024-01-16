ea6b509e0313ae7205568aa8955509e8d6b4e37a
Arzu Caner <arzuguneycaner@gmail.com>
2024-01-16 09:21:44 +0000

    Add profile information

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './intro.css';
+import bg from '../../assets/image.png';
+import btnImg from '../../assets/discover.png';
+import { Link } from 'react-scroll';
+
 
 const Intro = () => {
-  return (
-    <div>intro</div>
-  )
+    return (
+        <section id="intro">
+            <div className="introContent">
+                <span className="hello">Hello,</span>
+                <span className="introText">I'm<span className="introName">Arzu</span> <br />Software Developer</span>
+                <p className="introPara">I add colour to my creativity;I turn my dreams into reality with code!</p>
+                <Link><button className="btn"><img src={btnImg} alt="Discover" />Discover</button></Link>
+
+            </div>
+            <img src={bg} alt="Profile" className="bg" />
+        </section>
+    );
 }
 
 export default Intro;
