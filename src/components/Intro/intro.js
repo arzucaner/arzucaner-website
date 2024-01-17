c9e40aa081beb19705160aa9954155656f2c0183
Arzu Caner <arzuguneycaner@gmail.com>
2024-01-17 10:27:16 +0000

    Update intro.css

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -11,8 +11,8 @@ const Intro = () => {
             <div className="introContent">
                 <span className="hello">Hello,</span>
                 <span className="introText">I'm<span className="introName">Arzu</span> <br />Software Developer</span>
-                <p className="introPara">I add colour to my creativity;I turn my dreams into reality with code!</p>
-                <Link><button className="btn"><img src={btnImg} alt="Discover" />Discover</button></Link>
+                <p className="introPara">I add colour to my creativity; I turn my dreams into reality with code!</p>
+                <Link><button className="btn"><img src={btnImg} alt="Discover" className='btnImg' />Discover</button></Link>
 
             </div>
             <img src={bg} alt="Profile" className="bg" />
