47b3dc778380f093ecb8b4d1e13bf82da440aeb2
Arzu Caner <arzuguneycaner@gmail.com>
2024-02-24 22:22:40 +0000

    Add skills section

---
+++ src/components/Skills/skills.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './skills.css';
+import SkillsImage from '../../assets/skills.png';
+
+
+const Skills = () => {
+  return (
+    <section id='skills'>
+      <span className="skillTitle">Who I am</span>
+      <span xlassName="skillDesc">I'm Arzu, a passionate Software Developer with a strong technical SEO background.
+
+Tech Stack:
+
+Frontend: HTML, CSS, JavaScript, TypeScript, Vue.js, React.js⚛️
+Backend: Node.js, Testing with Jest 🃏
+Database: MongoDB, MySQL🐬
+Containerization: Docker 🐳
+I'm passionate about building robust and scalable applications. Let's connect and create an amazing project together! 🚀</span>
+   
+    </section>
+  );
+}
+
+export default Skills;
