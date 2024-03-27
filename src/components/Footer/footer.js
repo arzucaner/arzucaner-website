e105220bf15c6afbe0722bec22849d768a40be00
Arzu Caner <arzuguneycaner@gmail.com>
2024-03-27 23:54:24 +0000

    Add footer

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+     <Footer className='footer'>
+      Copyright &#169; 2024 Arzu Caner. All right received.
+     </Footer>
+   
+  );
+}
+
+export default Footer;
