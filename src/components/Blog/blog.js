4c9adc014aed480fa975caff242fbfdb95c8622a
Arzu Caner <arzuguneycaner@gmail.com>
2024-04-01 23:18:15 +0100

    Add blog CSS

--- src/components/Blog/blog.js
+++ src/components/Blog/blog.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './blog.css';
-import Blog from '../../components/Blog';
 import Blogimage1 from '../../assets/blogimage1.png';
 
 const Blog = () =>  {
   return (
     <section id='blog'>
-<h2 classname="blogTitle">My Blog</h2>
+<h2 className="blogTitle">My Blog</h2>
 <span className="blogDesc">Lorem</span>
        <div className="blogImgs">
         <img src={Blogimage1} alt="" className="blogImg" />
         </div>
-       <button className="workBtn">See More</button>
+       <button className="blogBtn">See More</button>
     </section>    
   );
 }
